a9777aaeb817b172f4debc7b30da2617623c3421
manhvannguyen1230-spec <manhvannguyen1230@gmail.com>
2026-04-10 15:53:05 +0700

    Increase environment exposure from 1.4 to 1.7

--- js/main.js
+++ js/main.js
@@ -4527,7 +4527,7 @@
 
             // Always set RoomEnvironment as base so scene is never dark
             renderer.toneMapping = THREE.ACESFilmicToneMapping;
-            renderer.toneMappingExposure = 1.4;
+            renderer.toneMappingExposure = 1.7;
             const roomEnv = new RoomEnvironment();
             const roomEnvMap = pmremGenerator.fromScene(roomEnv).texture;
             scene.environment = roomEnvMap;
