f802f0f158e579e8ffe5af858b23e105fe672e1a
manhvannguyen1230-spec <manhvannguyen1230@gmail.com>
2026-04-10 15:41:33 +0700

    Increase environment exposure to 2.0 for brighter scene

--- js/main.js
+++ js/main.js
@@ -4527,7 +4527,7 @@
 
             // Always set RoomEnvironment as base so scene is never dark
             renderer.toneMapping = THREE.ACESFilmicToneMapping;
-            renderer.toneMappingExposure = 1.0;
+            renderer.toneMappingExposure = 2.0;
             const roomEnv = new RoomEnvironment();
             const roomEnvMap = pmremGenerator.fromScene(roomEnv).texture;
             scene.environment = roomEnvMap;
